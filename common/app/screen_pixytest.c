328161602609e2b1f82fbf9897ee4002f77df6ce
t-moe <timolang@gmail.com>
2015-05-02 11:58:44 +0200

    Added some more touch functions. Improved pixy test. Drag the Image around!

--- common/app/screen_pixytest.c
+++ common/app/screen_pixytest.c
@@ -1,17 +1,55 @@
 #include "screen_pixytest.h"
 #include "button.h"
 #include "tft.h"
+#include "touch.h"
 #include "pixy.h"
 #include <stdlib.h>
 #include "system.h"
 
-static bool pixy_connected = false;
+static volatile bool pixy_connected = false;
 
 
 static BUTTON_STRUCT b_back;
+static TOUCH_AREA_STRUCT a_area;
 
 static void b_back_cb(void* button) {
         gui_screen_back();
+}
+
+POINT_STRUCT pixy_pos;
+POINT_STRUCT old_pos;
+static void touchCB(void* touchArea, TOUCH_ACTION triggeredAction) {
+	POINT_STRUCT p = touch_get_last_point();
+	switch(triggeredAction) {
+		case PEN_ENTER:
+                case PEN_DOWN:
+			old_pos = p;
+                break;
+                case PEN_MOVE:
+		{
+			int16_t deltaX = p.x - old_pos.x;
+			int16_t deltaY = p.y - old_pos.y;
+			old_pos=p;
+			printf("%d %d\n",deltaX,deltaY);
+			if(pixy_connected) {
+				int16_t new_x = pixy_pos.x+deltaX*2;
+				int16_t new_y = pixy_pos.y-deltaY*2;
+				if(new_x<0) new_x=0;
+				if(new_x>1000) new_x=1000;
+				if(new_y<0) new_y=0;
+				if(new_y>1000) new_y=1000;
+				pixy_pos.x = new_x;
+				pixy_pos.y= new_y;
+			}
+		}
+                break;
+		case PEN_UP:
+		case PEN_LEAVE:
+			printf("Leave/up\n");
+		default: break;
+        }
+
+	
 }
 
 
@@ -30,14 +68,27 @@ static void enter(void* screen) {
         b_back.callback=b_back_cb; //Call b_back_cb as Callback
         gui_button_add(&b_back); //Register Button (and run the callback from now on)
 
+	//Area test
+        a_area.hookedActions =  PEN_DOWN | PEN_MOVE |  PEN_ENTER | PEN_UP | PEN_LEAVE;
+        a_area.x1 = 0;
+        a_area.y1 = 0;
+        a_area.x2 = 317;
+        a_area.y2 = 197;
+        a_area.callback = touchCB;
+        touch_register_area(&a_area);
+
+
 
 	//Pixy stuff
         pixy_connected = (pixy_init()==0); //try to connect to pixy
-
+	if(pixy_connected) {
+		pixy_pos.x=pixy_pos.y=500;
+	}
 }
 
 static void leave(void* screen) {
 	gui_button_remove(&b_back);
+	touch_unregister_area(&a_area);
 }
 
 int pixy_led_test();
@@ -51,6 +102,7 @@ static void update(void* screen) {
                 pixy_close(); //Ensure that all pixy resources are freed (failsafe)
                 if(pixy_init()==0) { //try to connect to pixy
                         pixy_connected=true;
+			pixy_pos.x=pixy_pos.y=500;
 			printf("pixy reinitialized\n");
                 }
         }
@@ -66,6 +118,11 @@ static void update(void* screen) {
                 if(pixy_frame_test()!=0) {
                          pixy_connected=false;
                 }
+
+		
+		pixy_rcs_set_position(0,pixy_pos.x);
+		pixy_rcs_set_position(1,pixy_pos.y);
+
                 //system_delay(500);
         }
 }
